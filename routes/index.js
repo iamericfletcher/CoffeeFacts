e64a44631b56683ece3bdf63edcc2e1006e8d5d9
Eric Fletcher <64165327+iamericfletcher@users.noreply.github.com>
2023-10-22 20:33:42 -0400

    Update

--- routes/index.js
+++ routes/index.js
@@ -1,16 +1,21 @@
-var express = require('express');
-var axios = require('axios');
-var router = express.Router();
+let express = require('express');
+let axios = require('axios');
+let router = express.Router();
 
 
 /* GET home page. */
 router.get('/', async function (req, res, next) {
     try {
         const response = await axios.get('http://64.227.28.79:3002/facts');
-        res.render('index', { title: 'CoffeeFacts', facts: response.data });
+        if (response.data.length === 0) {
+            console.log('No facts in database');
+            res.render('index', { title: 'CoffeeFacts', facts: [{"fact":"No facts in database"}]});
+        } else {
+            res.render('index', { title: 'CoffeeFacts', facts: response.data });
+        }
     } catch (error) {
         console.log('Error fetching data:', error);
-        res.render('index', { title: 'CoffeeFacts', facts: [] });
+        res.render('index', { title: 'CoffeeFacts',  facts: [{"fact":"Error fetching data from database"}]});
     }
 });
 
